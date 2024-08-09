ef41780a90f5cef30e26a7bd8d8cb0d7afdca1fc
Ebalin1411 <ebalinrosecse2006@gmail.com>
2024-08-09 17:29:41 +0530

    mobile view nav bar design added

--- animated-portfolio/app/pages/home/page.tsx
+++ animated-portfolio/app/pages/home/page.tsx
@@ -16,22 +16,22 @@ const HeroSection = () => {
       <div className=" container mx-auto absolute w-full top-12 flex p-20 m-8 ">
         <div className="flex-1  mt-8 ">
           <div className="flex flex-col text-white gap-6 ">
-            <span className=" text-[24px] font-leckerli font-bold">
+            <span className="text-lg  md:text-[24px] font-leckerli font-bold">
               Hi there!
             </span>
             <span className="text-4xl  font-bold">
               I'm{" "}
-              <span className="text-[#0081FF] text-[40px] font-extrabold">
+              <span className="text-[#0081FF] text-lg md:text-[40px] font-extrabold">
                 Daniel Ray
               </span>
             </span>
-            <span className=" text-3xl">Graphic Designer</span>
-            <p className=" text-sm w-[450px] font[24px] ">
+            <span className="text-lg md:text-3xl">Graphic Designer</span>
+            <p className=" text-sm w-[250px] md:w-[350px]  lg:w-[650px] md:text-[14px] lg:text-[18px]">
               Welcome to a world where graphic and web design converge to create
               captivating digital experiences. Here, your vision comes to life
               with unrivaled creativity.
             </p>
-            <div className=" rounded-3xl shadow-md  bg-[#303030] text-sm   px-4 w-40 py-2 text-center">
+            <div className=" rounded-3xl shadow-md  bg-[#303030] text-[10px] md:text-sm lg:text-lg   px-4 w-40 py-2 text-center">
               {" "}
               ABOUT ME
             </div>
