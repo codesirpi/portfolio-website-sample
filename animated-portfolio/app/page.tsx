eb834dbd9256c795b1052f973789525275afaf6d
ajithkumar-kasi <ajithkumarkasi555@gmail.com>
2024-08-09 17:33:05 +0530

    added stacks page

--- animated-portfolio/app/page.tsx
+++ animated-portfolio/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { ClientsSection } from "./components/clients-section";
 import { ContactSection } from "./components/contact-section";
+import { StacksSection } from "./components/stacks-section";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between ">
       <ClientsSection />
+      <StacksSection />
       <ContactSection />
     </main>
   );
