0ef08ee979f035bcb94482e4af8d9569ee48a2f9
AJITHKUMAR <116996938+ajithkumar-kasi@users.noreply.github.com>
2024-08-09 14:10:13 +0530

    Navbar section (#4)
    * git push check
    
    * added nav bar section
    
    * finished client section without font style
    
    * added font style
    
    * added contact page

--- animated-portfolio/app/page.tsx
+++ animated-portfolio/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { ClientsSection } from "./components/clients-section";
+import { ContactSection } from "./components/contact-section";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
+    <main className="flex flex-col items-center justify-between ">
       <ClientsSection />
+      <ContactSection />
     </main>
   );
 }
